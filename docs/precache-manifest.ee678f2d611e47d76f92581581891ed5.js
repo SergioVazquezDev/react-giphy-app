ae65fdaae26ed0247e2de1c87024751a8b0562d6
Sergio Vazquez Arenas <sergio.vazquez@iteriam.es>
2022-01-29 21:12:38 +0100

    add docs

---
+++ docs/precache-manifest.ee678f2d611e47d76f92581581891ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51b8f14d7e3c22d7ca13cd32641b09b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8606f194fa4a50b5aaef",
+    "url": "/static/css/main.57965d85.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8606f194fa4a50b5aaef",
+    "url": "/static/js/main.ec33fffa.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
